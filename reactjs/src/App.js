31ab44724c16d0c0660f31fbc8ecb87b3ea135c9
Dirceu Heineck <dirceu.heineck@fw7.com.br>
2022-07-22 14:34:48 -0300

    feat(api): updated delete db

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,30 +1,39 @@
 import './App.css'
 import api from './services/api'
 import { format } from 'date-fns'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { FaTrashAlt, FaEdit, FaSave } from 'react-icons/fa'
+import axios from 'axios'
 
 export default function App() {
   const [employees, setEmployees] = useState()
 
-  useEffect(() => {
-    api
-      .get('/employees')
-      .then(response => setEmployees(response.data))
-      .catch(err => {
-        console.error('ops! ocorreu um erro' + err)
-      })
-  }, [])
+  api
+    .get('/employees')
+    .then(response => setEmployees(response.data))
+    .catch(err => {
+      console.error('ops! ocorreu um erro' + err)
+    })
+
+  const detele = async product => {
+    const { data } = await axios.delete(`http://localhost:3001/product`, {
+      params: { id: product }
+    })
+  }
 
   return (
-    <div class="styled-table">
+    <div className="styled-table">
       <table>
         <thead>
           <tr>
             <th>Código:</th>
             <th>Descrição:</th>
             <th>Tamanho:</th>
-            <th>EQuantidade:</th>
+            <th>Quantidade:</th>
             <th>Data:</th>
+            <th>Excluir:</th>
+            <th>Editar:</th>
+            <th>Salvar:</th>
           </tr>
         </thead>
         <tbody>
@@ -36,11 +45,26 @@ export default function App() {
 
               return (
                 <tr>
-                  <td> {employee.pr_codpro}</td>
+                  <td>{employee.pr_codpro}</td>
                   <td>{employee.pr_descri}</td>
                   <td>{employee.pr_tamanh}</td>
                   <td>{employee.pr_estatu}</td>
                   <td> {formattedDate}</td>
+                  <td>
+                    <center>
+                      <FaTrashAlt onClick={() => detele(employee.pr_codpro)} />
+                    </center>
+                  </td>
+                  <td>
+                    <center>
+                      <FaEdit />
+                    </center>
+                  </td>
+                  <td>
+                    <center>
+                      <FaSave />
+                    </center>
+                  </td>
                 </tr>
               )
             })}

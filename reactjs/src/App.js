f6c6fb1b501c14722d2435a02683f1203f584480
Dirceu Heineck <dirceu.heineck@fw7.com.br>
2022-07-22 08:49:05 -0300

    feat(api): update conection bd

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import api from './services/api'
 import './App.css'
+import { format } from 'date-fns'
 
 export default function App() {
   const [employees, setEmployees] = useState()
@@ -20,22 +21,26 @@ export default function App() {
         <thead>
           <tr>
             <th>Código:</th>
-            <th>Nome:</th>
-            <th>Empresa:</th>
-            <th>Cidade:</th>
-            <th>Idade:</th>
+            <th>Descrição:</th>
+            <th>Tamanho:</th>
+            <th>EQuantidade:</th>
+            <th>Data:</th>
           </tr>
         </thead>
         <tbody>
           {employees &&
             employees.map(employee => {
+              const date = new Date(employee.pr_datcri)
+
+              const formattedDate = format(date, 'dd/MM/yyyy')
+
               return (
                 <tr>
-                  <td> {employee.id}</td>
-                  <td>{employee.name}</td>
-                  <td>{employee.company}</td>
-                  <td>{employee.city}</td>
-                  <td>{employee.age}</td>
+                  <td> {employee.pr_codpro}</td>
+                  <td>{employee.pr_descri}</td>
+                  <td>{employee.pr_tamanh}</td>
+                  <td>{employee.pr_estatu}</td>
+                  <td> {formattedDate}</td>
                 </tr>
               )
             })}

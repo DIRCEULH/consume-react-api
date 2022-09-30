1b3bc14ff77f9a194d533b090b2d6fc7c8a98d44
Dirceu Heineck <dirceu.heineck@fw7.com.br>
2022-09-30 17:23:25 -0300

    feat(react): update layout pagination

--- reactjs/src/components/Home/index.js
+++ reactjs/src/components/Home/index.js
@@ -221,7 +221,7 @@ export default function Home() {
                 setCurrentPage(Number(e.target.value))
               }}
             >
-              {index}
+              {index + 1}
             </button>
           )
         })}
